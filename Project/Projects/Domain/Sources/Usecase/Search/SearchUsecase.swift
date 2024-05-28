b3ef61797e81aea418a8ada4706040312e170edf
Hanwe Lee <mobiledev@kakao.com>
2024-05-29 00:18:37 +0900

    rename: 오탈자 수정

--- Project/Projects/Domain/Sources/Usecase/Search/SearchUsecase.swift
+++ Project/Projects/Domain/Sources/Usecase/Search/SearchUsecase.swift
@@ -15,7 +15,7 @@ public protocol SearchUsecase {
   var isQuerying: Bool { get }
   
   /**
-   이 메소드들은 threa safe함을 보장하지 않습니다.
+   이 메소드들은 thread safe함을 보장하지 않습니다.
    */
   func searchBookList(keyword: String) async -> Result<[Book], Error>
   func moreBookList() async -> Result<[Book], Error>
